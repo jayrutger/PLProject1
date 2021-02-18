82b12ec4187ea1035370bede9db29634f0de6827
jayrutger <jar110@students.uwf.edu>
2021-02-18 14:18:34 -0600

    initial files

---
+++ main.c
@@ -0,0 +1,31 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	FILE * fp;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	fp = fopen("a1","r");
+	if(fp == NULL){
+		printf("fuck");
+		exit(EXIT_FAILURE);
+	}
+	while ((read = getline(&line,&len,fp)) != -1) {
+		printf("Retrieved line of length %zu:\n", read);
+		
+		char line2 = line[0];
+		printf("%c",line2);
+		//printf("%s",line);
+
+
+	}
+
+	fclose(fp);
+	if (line)
+		free(line);
+	exit(EXIT_SUCCESS);
+}
